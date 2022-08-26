61610952b5506cad4c6058b86b013c51864b1ed1
Gabor Nemeth <97887512+Gane02x@users.noreply.github.com>
2022-08-26 20:00:38 +0200

    Update ImageCtrl.ts

--- src/controllers/ImageCtrl.ts
+++ src/controllers/ImageCtrl.ts
@@ -5,28 +5,35 @@ import path from "path"
 
 export class ImageCtrl {
   async resizeImage(req: Request, res: Response) {
+    let error = ""
     const imgName = req.query.filename as unknown as string
     const width = req.query.width as unknown as string
     const height = req.query.height as unknown as string
 
     const outdir = `./images/res_${width}x${height}-${imgName}.jpg`
     const resizedImg = path.resolve(outdir)
-    if (!fs.existsSync(resizedImg)) {
-       const response = await Image.edit(outdir, imgName, width, height)
-       if (!response) {
-         //eslint-disable-next-line no-console, no-undef
-         console.log("No Image found!")
-         res.status(404).send("Image not found!")
-       }
+    
+    if ( Number(width) <= 0 || Number(height) <= 0 ) {
+      error = "Invalid paramaters. Width and/or height have to be a positive Integer"
+      res.status(400).send(error)
+    } else {
+      if (!fs.existsSync(resizedImg)) {
+         const response = await Image.edit(outdir, imgName, width, height)
+         if (!response) {
+           //eslint-disable-next-line no-console, no-undef
+           console.log("No Image found!")
+           res.status(404).send("Image not found!")
+         }
+      }
+      if (!imgName || !width || !height) {
+        res
+          .status(400)
+          .send(
+            "Request query is missing - please make sure you are using the correct url pattern: http://localhost:3000/api/images?filename=FILENAME&width=YOURWIDTH&height=YOURHEIGHT"
+          )
+      }
+      res.status(200).send(resizedImg)
     }
-    if (!imgName || !width || !height) {
-      res
-        .status(400)
-        .send(
-          "Request query is missing - please make sure you are using the correct url pattern: http://localhost:3000/api/images?filename=FILENAME&width=YOURWIDTH&height=YOURHEIGHT"
-        )
-    }
-    res.status(200).send(resizedImg)
   }
 }
 export default new ImageCtrl()
